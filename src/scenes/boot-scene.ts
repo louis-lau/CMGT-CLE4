dae4d834bf1c78fa7ed2bf6b2f7ea4f7b317a88e
Louis Laureys <louis@laureys.me>
2019-05-24 00:41:54 +0200

    Use tiled map editor

--- src/scenes/boot-scene.ts
+++ src/scenes/boot-scene.ts
@@ -12,11 +12,6 @@ export class BootScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.image('alcohol', require('../assets/img/alcohol.png'))
-        this.load.image('chocolate', require('../assets/img/chocolate.png'))
-        this.load.image('cola-can', require('../assets/img/cola-can.png'))
-        this.load.image('fries', require('../assets/img/fries.png'))
-        this.load.image('fry', require('../assets/img/fry.png'))
         this.load.image('green-shit', require('../assets/img/green-shit.png'))
         this.load.image('parrot-pigeon', require('../assets/img/parrot-pigeon.png'))
         this.load.image('pigeon', require('../assets/img/pigeon.png'))
@@ -26,6 +21,8 @@ export class BootScene extends Phaser.Scene {
         this.load.image('flat', require('../assets/img/flat.png'))
         this.load.image('house1', require('../assets/img/house1.png'))
         this.load.image('house2', require('../assets/img/house2.png'))
+        this.load.image("tileset", require("../assets/tileset.png"));
+        this.load.tilemapTiledJSON("map-city", require("../assets/maps/city.json"));
 
         this.load.on('complete', () => {
             console.log("everything is loaded")
--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -19,35 +19,23 @@ export class GameScene extends Phaser.Scene {
     }
 
     create(): void {
-        this.physics.world.bounds.width = 5000
-
-        this.add.tileSprite(0, 0, this.physics.world.bounds.width, 270, 'city-sky').setOrigin(0);
-        this.flats = this.add.tileSprite(50, 117, this.physics.world.bounds.width, 270, 'flat').setOrigin(0);
-        this.house1 = this.add.tileSprite(100, 223, this.physics.world.bounds.width, 270, 'house1').setOrigin(0);
-        this.house2 = this.add.tileSprite(300, 223, this.physics.world.bounds.width, 270, 'house2').setOrigin(0);
-
+        // Create map and tileset from loaded json and image
+        const map = this.make.tilemap({ key: "map-city" });
+        const tileset = map.addTilesetImage("tileset");
+        
+        // Set bounds width to the width of the loaded map
+        this.physics.world.bounds.width = map.widthInPixels
+        
+        this.add.tileSprite(0, 0, this.physics.world.bounds.width, this.physics.world.bounds.height, 'city-sky').setOrigin(0);
+        this.flats = this.add.tileSprite(0, 117, this.physics.world.bounds.width, this.physics.world.bounds.height, 'flat').setOrigin(0);
+        this.house1 = this.add.tileSprite(0, 223, this.physics.world.bounds.width, this.physics.world.bounds.height, 'house1').setOrigin(0);
+        this.house2 = this.add.tileSprite(0, 223, this.physics.world.bounds.width, this.physics.world.bounds.height, 'house2').setOrigin(0);
+        
+        const mainLayer = map.createStaticLayer("Tile Layer 1", tileset, 0, 0.28125);
         this.player = new Player(this)
 
-        this.cameras.main.setSize(480, 270)          // canvas size
-        this.cameras.main.setBounds(0, 0, this.physics.world.bounds.width, 270) // world size
-        this.cameras.main.startFollow(this.player, true, 1, 1, -175)
-
-
-        // 11 cans
-        this.food = this.physics.add.group({
-            key: 'cola-can',
-            repeat: 11,
-            setXY: { x: 12, y: 30, stepX: 70 },
-        })
-
-    }
-
-    private collectStar(player : Player , star) : void {
-        this.food.remove(star, true, true)
-        this.registry.values.score++
-
-        // TO DO check if we have all the stars, then go to the end scene
-    
+        this.cameras.main.setBounds(0, 0, this.physics.world.bounds.width, this.physics.world.bounds.height) // world size
+        this.cameras.main.startFollow(this.player, true, undefined, undefined, -175)
     }
 
     update(){
