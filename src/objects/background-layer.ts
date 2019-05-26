6358f513103e558bc9a24fb6cb3e3ea548ee7be8
Louis Laureys <louis@laureys.me>
2019-05-26 15:53:39 +0200

    Add background layer class

---
+++ src/objects/background-layer.ts
@@ -0,0 +1,31 @@
+export class BackgroundLayer extends Phaser.GameObjects.TileSprite {
+    private _speed: number
+
+    /**
+     * Create a new repeating background, can be parallax.
+     * 
+     * @param scene The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
+     * @param texture What texture to use for the background.
+     * @param yOffset Optional distance from top of texture to top of scene.
+     * @param speed Optional speed of the parralax effect.
+     */
+    constructor(scene: Phaser.Scene, texture: string, yOffset = 0, speed = 0) {
+        super(scene, 0, yOffset, scene.physics.world.bounds.width, scene.physics.world.bounds.height, texture);
+        
+        this.scene.add.existing(this)
+        this.setOrigin(0)
+        this._speed = speed;
+    }
+
+    public update() {
+        this.tilePositionX -= this.speed;
+    }
+
+    public get speed() {
+        return this._speed
+    }
+
+    public set speed(newSpeed: number) {
+        this._speed = newSpeed
+    }
+}
